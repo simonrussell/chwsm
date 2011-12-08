c278b6a318322acdf8b9d4890864cc947af16008
Simon Russell <simon@bellyphant.com>
2011-12-08 16:01:32 +1100

    more fiddling.

--- server2.c
+++ server2.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-typedef int socket_t;
+#include <ev.h>
 
 #define STRINGIZE_DETAIL(x) #x
 #define STRINGIZE(x) STRINGIZE_DETAIL(x)
@@ -28,22 +30,57 @@ int bind_socket(int socket, uint16_t port)
   return bind(socket, (struct sockaddr *) &address, sizeof(address));
 }
 
-int main(void)
+void set_nonblock(int socket)
 {
-  socket_t listener = socket(AF_INET, SOCK_STREAM, 0);
-  DIE_LZ(listener);
-  
-  DIE_LZ(bind_socket(listener, 9000));
-  DIE_LZ(listen(listener, 0));
+  fcntl(socket, F_SETFL, fcntl(socket, F_GETFL, 0) | O_NONBLOCK);
+}
 
-  socket_t connection = accept(listener, NULL, 0);
+int setup_listener(int port)
+{
+  int listener = socket(AF_INET, SOCK_STREAM, 0);
+  DIE_LZ(listener);
+
+  set_nonblock(listener);
+  
+  DIE_LZ(bind_socket(listener, port));
+  DIE_LZ(listen(listener, 100));
+  
+  return listener;
+}
+
+void listener_callback(EV_P_ ev_io *w, int revents)
+{
+  int connection = accept(w->fd, NULL, 0);
+
+  if (connection < 0 && errno == EWOULDBLOCK)
+    return;
+    
   DIE_LZ(connection);  
  
-  write(connection, "Hello World\n", 12); 
- 
-  DIE_LZ(shutdown(connection, SHUT_RDWR));
-  DIE_LZ(close(connection));
+  set_nonblock(connection);
   
+  puts("accepted!");
+
+  char message[] = "HTTP/1.0 200 OK\n\nHello world";
+  write(connection, message, sizeof(message)); 
+ 
+//    DIE_LZ(shutdown(connection, SHUT_WR));
+  DIE_LZ(close(connection));
+}
+
+int main(void)
+{
+  int listener = setup_listener(9000);
+  struct ev_loop *loop = EV_DEFAULT;
+
+  ev_io listener_watcher;
+  ev_io_init(&listener_watcher, listener_callback, listener, EV_READ);
+  ev_io_start(loop, &listener_watcher);
+  
+  ev_run(loop, 0);
+
+  puts("finishing loop");
+
   DIE_LZ(shutdown(listener, SHUT_RDWR));
   DIE_LZ(close(listener));
 
